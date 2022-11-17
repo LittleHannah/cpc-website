80d56877743a016df8d126ebc685cb0df2176037
MorganHis <18811692831@163.com>
2022-11-17 17:59:08 +0800

    1 注释掉搜索栏 2 设置点击图片跳转课题组网站

--- src/components/GroupNav.vue
+++ src/components/GroupNav.vue
@@ -2,22 +2,31 @@
   <nav class="navbar navbar-dark my-bg-dark">
     <div class="container-fluid" style="width:1200px">
       <div style="display:flex;width: 50%;align-items: center;">
-        <img style="width: 150px;height: 32px;margin-right: 12px;" src="@/assets/img/logo.jpg" alt="HumPOG-logo"/>
+        <img style="width: 150px;height: 32px;margin-right: 12px;" @click="docs" src="@/assets/img/logo.jpg" alt="HumPOG-logo"/>
         <a class="navbar-brand brand-small">
           <p style="margin:0;line-height:1.2rem;font-size: 1.1rem;font-weight: 500;">Human Population<br> Omics Group</p>
         </a>
       </div>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button type="button" class="btn btn-outline-primary">Search</button>
-      </form>
+<!--      先把搜索栏注释掉-->
+<!--      <form class="d-flex">-->
+<!--        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">-->
+<!--        <button type="button" class="btn btn-outline-primary">Search</button>-->
+<!--      </form>-->
+
     </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'GroupNav'
+  name: 'GroupNav',
+  methods: {
+    docs() {
+      // console.log("转到课题组网站");
+      window.location.href = 'https://pog.fudan.edu.cn';
+    },
+  },
+
 }
 </script>
 
