ce72117ed67fbbc0e0cb981ba8f6988956710d61
MorganHis <18811692831@163.com>
2022-11-21 10:26:38 +0800

    首页搞张图上去

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-
+    <div style="max-width: 1200px; padding: 0 10px 20px">
+    <div style="display:flex;justify-content:space-between;margin-bottom: 10px;margin-top: 20px;">
+          <img data-aos="fade-right" src="@/assets/img/cpcHome.png" style="width:100%;border-radius: 10px;">
+        </div>
   </div>
 </template>
 
