2664c87fc469557377ba754bc029d9c8744157b4
Xiaohan Zhao <zhao_xiaohan@126.com>
2023-01-07 09:33:12 +0800

    added file link

--- src/views/data/index.vue
+++ src/views/data/index.vue
@@ -4,13 +4,23 @@
     All the codes needed to reproduce the results from this work can be found on GitHub, <a href="https://github.com/orgs/Shuhua-Group/repositories">https://github.com/orgs/Shuhua-Group/repositories</a>.
     <h2>The CPC Phase I Data Availability</h2>
     The release of the CPC Phase I data by this work is permitted by The Ministry of Science and Technology of the People’s Republic of China (permission no. 2022BAT2142, 2022BAT1947, and 2022BAT1948) at the National Genomics Data Center  (<a href="https://ngdc.cncb.ac.cn">https://ngdc.cncb.ac.cn</a>).
+    <div>
+      <a :href="fileUrl1" download="CPC.Phase1.CHM13v2.gfa.gz" target="_blank">
+        CPC.Phase1.CHM13v2.gfa.gz
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
-// export default {
-//   name: 'Data'
-// }
+
+export default {
+  data(){
+    return {
+      fileUrl1: `https://pog.fudan.edu.cn/cpc/files/CPC.Phase1.CHM13v2.gfa.gz`
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
