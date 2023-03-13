ed719fb5a7d0b1e9b698dc0ec0e5b02c994e5a59
MorganHis <18811692831@163.com>
2023-03-13 10:04:30 +0800

    cpc会议信息更新-华大第二届大人群基因组研究鼎峰论坛-20230313

--- src/views/conferenceTalks/index.vue
+++ src/views/conferenceTalks/index.vue
@@ -3,7 +3,6 @@
     <div style="max-width: 1200px; padding: 0 10px 20px">
 
       <div class="card-content">
-<!--        <h2>THE 8TH N.GS ORGANIZATION INNOVATION DEVELOPER CONFERENCE</h2>-->
         <h2>the 8th NGSer Conference</h2>
 
       </div>
@@ -14,7 +13,17 @@
         <img data-aos="fade-right" src="@/assets/img/conference/Conference1.jpeg"
              style="width:80%;border-radius: 10px;">
       </div>
+      <div class="card-content">
+        <h2>the Second Summit Forum on Large Population Genome Research</h2>
 
+      </div>
+      <p>
+        Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the Second Summit Forum on Large Population Genome Research on Mar 11, 2023.
+      </p>
+      <div style="display:flex;justify-content:space-between;margin-bottom: 40px;">
+        <img data-aos="fade-right" src="@/assets/img/conference/Conference2.jpg"
+             style="width:80%;border-radius: 10px;">
+      </div>
     </div>
   </div>
 </template>
