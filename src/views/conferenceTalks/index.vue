45c0e073c31f0f3b7d2c220d36518c35c49dd526
MorganHis <18811692831@163.com>
2023-05-04 14:53:29 +0800

    会议信息页面版式更新

--- src/views/conferenceTalks/index.vue
+++ src/views/conferenceTalks/index.vue
@@ -3,9 +3,19 @@
     <div style="max-width: 1200px; padding: 0 10px 20px">
 
       <!--会议3-->
-      <div class="card-content">
-        <h2>the 18th Annual Meeting of the International Conference on Genomics</h2>
+      <!--      <div class="card-content">-->
+      <!--        <h2>the 18th Annual Meeting of the International Conference on Genomics</h2>-->
 
+      <!--      </div>-->
+      <!--        <div class="card2" >-->
+      <!--            the 18th Annual Meeting of the International Conference on Genomics-->
+      <!--        </div>-->
+      <div style="font-size: 20px;font-weight: 600;padding: 10px 0 0">
+        <div style="background-color: #2b4275;border-radius: 5px;padding: 0 0 0;width: fit-content">
+          <p style="color: #ffffff;padding: 5px 10px 5px;">
+            the 18th Annual Meeting of the International Conference on Genomics
+          </p>
+        </div>
       </div>
       <p>
         Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the ICG-18 on April 27th, 2023.
@@ -16,9 +26,18 @@
       </div>
 
       <!--会议2-->
-      <div class="card-content">
-        <h2>the Second Summit Forum on Large Population Genome Research</h2>
+      <!--      <div class="card-content">-->
+      <!--        <h2>the Second Summit Forum on Large Population Genome Research</h2>-->
+      <!--      </div>-->
+      <!--      <div style="font-size: 20px;font-weight: 500;padding: 10px 0 0">-->
+      <!--      <div style="background-color: rgba(100,149,237,0.18);border-radius: 5px;padding: 0 0 0;width: fit-content">-->
 
+      <div style="font-size: 20px;font-weight: 600;padding: 10px 0 0">
+        <div style="background-color: #2b4275;border-radius: 5px;padding: 0 0 0;width: fit-content">
+          <p style="color: #ffffff;padding: 5px 10px 5px;">
+            the Second Summit Forum on Large Population Genome Research
+          </p>
+        </div>
       </div>
       <p>
         Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the Second Summit Forum on Large Population Genome
@@ -30,9 +49,15 @@
       </div>
 
       <!--会议1-->
-      <div class="card-content">
-        <h2>the 8th NGSer Conference</h2>
-
+      <!--      <div class="card-content">-->
+      <!--        <h2>the 8th NGSer Conference</h2>-->
+      <!--      </div>-->
+      <div style="font-size: 20px;font-weight: 600;padding: 10px 0 0">
+        <div style="background-color: #2b4275;border-radius: 5px;padding: 0 0 0;width: fit-content">
+          <p style="color: #ffffff;padding: 5px 10px 5px;">
+            the 8th NGSer Conference
+          </p>
+        </div>
       </div>
       <p>
         Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the 8th NGSer conference on Nov 19, 2022.
@@ -61,4 +86,31 @@
 p {
   color: #606266;
 }
+
+//.card2::before {
+//  display: inline-block;
+//  content: '';
+//  width: 4px;
+//  height: 20px;
+//  margin-right: 10px;
+//  border-radius: 1px;
+//  //background-color: var(--subtext-color);
+//  background-color: #f8f9fa;
+//}
+//
+//.card2 {
+//  //background-color: rgb(248, 249, 250);
+//  background-color: #2b4275;
+//  //background-color: #3a5285;
+//  //background-color: #7284b7;
+//
+//  width: fit-content;
+//  border-radius: 5px;
+//  height: 60px;
+//  color: #ffffff;
+//  line-height: 60px;
+//  font-size: 28px;
+//  font-weight: 540;
+//  margin: 20px 0;
+//}
 </style>
