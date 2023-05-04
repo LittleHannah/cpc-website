d52201738c50a743819d8b3b382fee5e81b4be65
MorganHis <18811692831@163.com>
2023-05-04 11:13:56 +0800

    会议信息更新ICG-18

--- src/views/conferenceTalks/index.vue
+++ src/views/conferenceTalks/index.vue
@@ -2,6 +2,34 @@
   <div>
     <div style="max-width: 1200px; padding: 0 10px 20px">
 
+      <!--会议3-->
+      <div class="card-content">
+        <h2>the 18th Annual Meeting of the International Conference on Genomics</h2>
+
+      </div>
+      <p>
+        Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the ICG-18 on April 28th, 2023.
+      </p>
+      <div style="display:flex;justify-content:space-between;margin-bottom: 40px;">
+        <img data-aos="fade-right" src="@/assets/img/conference/Conference3.jpeg"
+             style="width:80%;border-radius: 10px;">
+      </div>
+
+      <!--会议2-->
+      <div class="card-content">
+        <h2>the Second Summit Forum on Large Population Genome Research</h2>
+
+      </div>
+      <p>
+        Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the Second Summit Forum on Large Population Genome
+        Research on Mar 11, 2023.
+      </p>
+      <div style="display:flex;justify-content:space-between;margin-bottom: 40px;">
+        <img data-aos="fade-right" src="@/assets/img/conference/Conference2.jpg"
+             style="width:80%;border-radius: 10px;">
+      </div>
+
+      <!--会议1-->
       <div class="card-content">
         <h2>the 8th NGSer Conference</h2>
 
@@ -13,17 +41,8 @@
         <img data-aos="fade-right" src="@/assets/img/conference/Conference1.jpeg"
              style="width:80%;border-radius: 10px;">
       </div>
-      <div class="card-content">
-        <h2>the Second Summit Forum on Large Population Genome Research</h2>
 
-      </div>
-      <p>
-        Dr. Shuhua Xu gave a talk on CPC Phase I data analysis in the Second Summit Forum on Large Population Genome Research on Mar 11, 2023.
-      </p>
-      <div style="display:flex;justify-content:space-between;margin-bottom: 40px;">
-        <img data-aos="fade-right" src="@/assets/img/conference/Conference2.jpg"
-             style="width:80%;border-radius: 10px;">
-      </div>
+
     </div>
   </div>
 </template>
