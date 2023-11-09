e66ffed7a3489b1a4fb3d48ca5b03a199e657d74
MorganHis <18811692831@163.com>
2023-11-09 21:40:28 +0800

    data页面CPC+HPRC泛基因组图处理后的SV集合

--- src/views/data/index.vue
+++ src/views/data/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div style="padding: 0px 10px 20px">
     <h2>Code Availability</h2>
-    All the codes needed to reproduce the results from this work can be found on GitHub, <a href="https://github.com/orgs/Shuhua-Group/repositories">https://github.com/orgs/Shuhua-Group/repositories</a>.
+    All the codes needed to reproduce the results from this work can be found on GitHub, <a
+      href="https://github.com/orgs/Shuhua-Group/repositories">https://github.com/orgs/Shuhua-Group/repositories</a>.
     <h2>The CPC Phase I Data Availability</h2>
-    The release of the CPC Phase I data by this work is permitted by The Ministry of Science and Technology of the People’s Republic of China at the National Genomics Data Center  (<a href="https://ngdc.cncb.ac.cn">https://ngdc.cncb.ac.cn</a>).
+    The release of the CPC Phase I data by this work is permitted by The Ministry of Science and Technology of the
+    People’s Republic of China at the National Genomics Data Center (<a href="https://ngdc.cncb.ac.cn">https://ngdc.cncb.ac.cn</a>).
 
     <h3 style="margin:20px 0">CPC pangenome reference</h3>
     <div style="padding-left:20px" v-for="c in cpc" :key="c">
@@ -13,10 +15,6 @@
           <a :href='`${fileUrl}/${f.class}/${f.name}`' :download='f.name' target="_blank">
             {{ f.name }}
           </a>
-
-<!--          <a :download='f.name' target="_blank" disabled>-->
-<!--            {{ f.name }}-->
-<!--          </a>-->
         </li>
       </ul>
     </div>
@@ -28,10 +26,6 @@
           <a :href='`${fileUrl}/${f.class}/${f.name}`' :download='f.name' target="_blank">
             {{ f.name }}
           </a>
-
-<!--          <a :download='f.name' target="_blank" disabled="">-->
-<!--            {{ f.name }}-->
-<!--          </a>-->
         </li>
       </ul>
     </div>
@@ -43,18 +37,40 @@
           <a :href='`${fileUrl}/${f.class}/${f.name}`' :download='f.name' target="_blank">
             {{ f.name }}
           </a>
-
-          <!--          <a :download='f.name' target="_blank" disabled="">-->
-          <!--            {{ f.name }}-->
-          <!--          </a>-->
         </li>
       </ul>
     </div>
-    * CPC pangenome reference includes 122 haplotypes of 61 samples from 36 East Asian populations.<br>
-    * CPC & HPRC pangenome reference includes all samples in CPC.Phase1 and HPRC.<br>
-    * Files with "-full" contain all contigs that could be assigned a chromosome, nothing masked or clipped.<br>
-    * Files with "-min.af.0.1" were clipped to keep only nodes covered by >= 10% haplotypes (only suitable for short-read mapping).<br>
-    * Files with neither suffix were clipped so that path intervals in the graph that span >=10kb without aligning to the minigraph are removed.
+
+    <h3 style="margin:20px 0">SV file of CPC & HPRC pangenome processed by the <a
+        href="https://github.com/Shuhua-Group/PanGenome_VCF_PostProcess" style="color: #4343d0;font-size: 23px">PanGenome_VCF_PostProcess</a>
+    </h3>
+    <div style="padding-left:20px" v-for="c in cpcSV" :key="c">
+      <h4>{{ c }}</h4>
+      <div style="margin-left: 15px;color: #383d48">
+        We have updated the process strategy about the complex loci with both small variants and SVs alleles, so that
+        the number of variants is slightly different than in the paper.<br>
+      </div>
+
+      <ul>
+        <li v-for="f in files" :key="f.name" v-if="f.class===c">
+          <a :href='`${fileUrl}/${f.class}/${f.name}`' :download='f.name' target="_blank">
+            {{ f.name }}
+          </a>
+        </li>
+      </ul>
+    </div>
+
+
+    <div style="margin-top: 50px">
+      * CPC pangenome reference includes 122 haplotypes of 61 samples from 36 East Asian populations.<br>
+      * CPC & HPRC pangenome reference includes all samples in CPC.Phase1 and HPRC.<br>
+      * Files with "-full" contain all contigs that could be assigned a chromosome, nothing masked or clipped.<br>
+      * Files with "-min.af.0.1" were clipped to keep only nodes covered by >= 10% haplotypes (only suitable for
+      short-read mapping).<br>
+      * Files with neither suffix were clipped so that path intervals in the graph that span >=10kb without aligning to
+      the minigraph are removed.
+    </div>
+
 
   </div>
 </template>
@@ -62,51 +78,55 @@
 <script>
 
 export default {
-  data(){
+  data() {
     return {
       fileUrl: `https://pog.fudan.edu.cn/cpc/files`,
 
       cpc: ['CPC.Phase1.CHM13v2-full', 'CPC.Phase1.CHM13v2', 'CPC.Phase1.CHM13v2-minaf.0.1'],
       cpchprc: ['CPC.HPRC.Phase1.CHM13v2', 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'],
       cpcGfff: ['CPC.p1.58inds.GFF3'],
+      cpcSV: ['CPC.HPRC.Phase1.processed.SVs.normed'],
       files: [
-        { name: 'CPC.Phase1.CHM13v2-full.gfa.gz', class: 'CPC.Phase1.CHM13v2-full'},
-        { name: 'CPC.Phase1.CHM13v2-full.gbwt', class: 'CPC.Phase1.CHM13v2-full'},
-        { name: 'CPC.Phase1.CHM13v2-full.gg', class: 'CPC.Phase1.CHM13v2-full'},
-        { name: 'CPC.Phase1.CHM13v2-full.xg', class: 'CPC.Phase1.CHM13v2-full'},
+        {name: 'CPC.Phase1.CHM13v2-full.gfa.gz', class: 'CPC.Phase1.CHM13v2-full'},
+        {name: 'CPC.Phase1.CHM13v2-full.gbwt', class: 'CPC.Phase1.CHM13v2-full'},
+        {name: 'CPC.Phase1.CHM13v2-full.gg', class: 'CPC.Phase1.CHM13v2-full'},
+        {name: 'CPC.Phase1.CHM13v2-full.xg', class: 'CPC.Phase1.CHM13v2-full'},
 
-        { name: 'CPC.Phase1.CHM13v2.gfa.gz', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.dist', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.gbwt', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.gg', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.min', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.xg', class: 'CPC.Phase1.CHM13v2'},
-        { name: 'CPC.Phase1.CHM13v2.vcf.gz', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.gfa.gz', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.dist', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.gbwt', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.gg', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.min', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.xg', class: 'CPC.Phase1.CHM13v2'},
+        {name: 'CPC.Phase1.CHM13v2.vcf.gz', class: 'CPC.Phase1.CHM13v2'},
 
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.gfa.gz', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.dist', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.gbwt', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.gg', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.min', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
-        { name: 'CPC.Phase1.CHM13v2-minaf.0.1.xg', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.gfa.gz', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.dist', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.gbwt', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.gg', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.min', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.Phase1.CHM13v2-minaf.0.1.xg', class: 'CPC.Phase1.CHM13v2-minaf.0.1'},
 
-        { name: 'CPC.HPRC.Phase1.CHM13v2.gfa.gz', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.dist', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.gbwt', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.gg', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.min', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.xg', class: 'CPC.HPRC.Phase1.CHM13v2'},
-        { name: 'CPC.HPRC.Phase1.CHM13v2.vcf.gz', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.gfa.gz', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.dist', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.gbwt', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.gg', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.min', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.xg', class: 'CPC.HPRC.Phase1.CHM13v2'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2.vcf.gz', class: 'CPC.HPRC.Phase1.CHM13v2'},
 
 
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gfa.gz', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.dist', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gbwt', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gg', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.min', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
-        { name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.xg', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1' },
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gfa.gz', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.dist', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gbwt', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.gg', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.min', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
+        {name: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1.xg', class: 'CPC.HPRC.Phase1.CHM13v2-minaf.0.1'},
 
-        { name: 'CPC.p1.58inds.GFF3.tar.gz', class: 'CPC.p1.58inds.GFF3' },
+        {name: 'CPC.p1.58inds.GFF3.tar.gz', class: 'CPC.p1.58inds.GFF3'},
+
+        {name: 'CPC.HPRC.Phase1.processed.SVs.normed.vcf.gz', class: 'CPC.HPRC.Phase1.processed.SVs.normed'},
+
       ]
     }
   }
@@ -115,6 +135,7 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/scss/index.scss';
+
 .card-title {
   margin-left: 8px;
   font-size: 20px;
